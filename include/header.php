da100b6818a5ff03a3a72addbca887b92a4193bf
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2022-10-11 13:02:33 +0500

    New Changes

---
+++ include/header.php
@@ -0,0 +1,19 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="<?php echo $appurl; ?>assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo $appurl; ?>assets/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="<?php echo $appurl; ?>assets/css/owl.carousel.min.css">
+<link rel="stylesheet" href="<?php echo $appurl; ?>assets/css/magnific-popup.css">
+<link rel="stylesheet" href="<?php echo $appurl; ?>assets/css/animate.css">
+<link rel="stylesheet" href="<?php echo $appurl; ?>assets/css/boxicons.min.css">
+<link rel="stylesheet" href="<?php echo $appurl; ?>assets/css/flaticon.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/jquery.slick/1.4.1/slick.css">
+<link rel="stylesheet" href="<?php echo $appurl; ?>assets/css/meanmenu.css">
+<link rel="stylesheet" href="<?php echo $appurl; ?>assets/css/nice-select.css">
+<link rel="stylesheet" href="<?php echo $appurl; ?>assets/css/odometer.css">
+<link rel="stylesheet" href="<?php echo $appurl; ?>assets/css/style.css">
+<link rel="stylesheet" href="<?php echo $appurl; ?>assets/css/time.css">
+<link rel="stylesheet" href="<?php echo $appurl; ?>assets/css/responsive.css">
+<script src="<?php echo $appurl; ?>assets/js/jquery-3.5.1.slim.min.js"></script>
+<link rel="icon" type="image/png" href="<?php echo $appurl; ?>assets/img/images/fav.png">    
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  
