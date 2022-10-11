30c82ca114f9c07da5f121234b9f374e687b4533
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2022-10-11 16:14:17 +0500

    New Changes

---
+++ include/categories_mobile.php
@@ -0,0 +1,59 @@
+<section class="card-slide">
+   <div class="container-homepage">
+   <div class="section-title section-title-five">
+      <h2 class="text-dark">Simple. Online. Transparent.</h2>
+      <h5 style="font-weight: 400;">Buying insurance has never been this simple (and enjoyable)!</h5>
+   </div>
+   <div class="row">
+      <!-- Контент -->
+         <div class="col-md-6 col-lg-6 col-sm-6">
+            <div class="card slider-card border-0">
+               <div class="card-body text-center">
+                  <div class="simple-online-transparent-slider">
+                     <img src="assets/img/images/family.png">
+                  </div>
+                  <div class="slider-heading">
+                     <h2><span>Life</span> Insurance</h2>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6 col-lg-6 col-sm-6">
+            <div class="card slider-card border-0">
+               <div class="card-body text-center">
+                  <div class="simple-online-transparent-slider">
+                     <img src="assets/img/images/bed.png">
+                  </div>
+                  <div class="slider-heading">
+                     <h2><span>Critical Illness</span> Insurance</h2>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6 col-lg-6 col-sm-6">
+            <div class="card slider-card border-0">
+               <div class="card-body text-center">
+                  <div class="simple-online-transparent-slider">
+                     <img src="assets/img/images/disability.png">
+                  </div>
+                  <div class="slider-heading">
+                     <h2><span>Disability</span> Insurance</h2>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6 col-lg-6 col-sm-6">
+            <div class="card slider-card border-0">
+               <div class="card-body text-center">
+                  <div class="simple-online-transparent-slider">
+                     <img src="assets/img/images/health.png">
+                  </div>
+                  <div class="slider-heading">
+                     <h2><span>Health</span> Insurance</h2>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+   </div>
+</section>
