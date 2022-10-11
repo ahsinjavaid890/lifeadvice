ac7f7a37a09a77bf8e9a34295304aa9c72920dbd
MalikAbubakar890 <96823154+MalikAbubakar890@users.noreply.github.com>
2022-10-11 17:02:49 +0500

    style.css, db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-$db = mysqli_connect("localhost","root","root","lifeadvice");
+$db = mysqli_connect("localhost","root","","lifeadvice");
 if (mysqli_connect_errno())
 {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
